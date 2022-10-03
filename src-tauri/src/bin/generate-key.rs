0ec14effd074d6c0e4417ca8bbad6c20c8eddf9e
hef <hef@pbrfrat.com>
2022-10-03 00:28:30 -0500

    fix(fmt): cargo fmt

--- src-tauri/src/bin/generate-key.rs
+++ src-tauri/src/bin/generate-key.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Write};
 use libp2p::identity::ed25519::Keypair;
+use std::io::{self, Write};
 fn main() -> io::Result<()> {
     let raw_key = Keypair::generate().encode();
     io::stdout().write_all(&raw_key)?;
--- src-tauri/src/bin/server.rs
+++ src-tauri/src/bin/server.rs
@@ -1,19 +1,20 @@
 #[cfg(not(target_family = "windows"))]
-use std::{env, os::unix::prelude::OsStrExt};
-#[cfg(not(target_family = "windows"))]
-use app::state::Stuff;
-#[cfg(not(target_family = "windows"))]
-use tokio::time::{sleep, Duration};
+use {
+    app::state::Stuff,
+    std::{env, os::unix::prelude::OsStrExt},
+    tokio::time::{sleep, Duration},
+};
 
 #[cfg(not(target_family = "windows"))]
 #[tokio::main]
 async fn main() {
-
-    let identity : libp2p::identity::Keypair;
+    let identity: libp2p::identity::Keypair;
     let server_identity_env_var = env::var_os("SERVER_IDENTITY");
     if let Some(server_identity) = server_identity_env_var {
         let mut server_identity_copy = server_identity.as_bytes().to_vec();
-        identity = libp2p::identity::Keypair::Ed25519(libp2p::identity::ed25519::Keypair::decode(&mut server_identity_copy).unwrap());
+        identity = libp2p::identity::Keypair::Ed25519(
+            libp2p::identity::ed25519::Keypair::decode(&mut server_identity_copy).unwrap(),
+        );
     } else {
         identity = libp2p::identity::Keypair::generate_ed25519();
     }
